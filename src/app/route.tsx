d8adaf387fe04c282373502b690864d29fcbe150
Samuel Newman <mozzius@protonmail.com>
2025-09-01 22:34:39 +0300

    formatting

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -1,20 +1,20 @@
-import { NextRequest } from "next/server";
+import { NextRequest } from 'next/server';
 import QRCode from 'qrcode';
 
 export const dynamic = 'force-dynamic'
 
 export async function GET(request: NextRequest) {
-const url = request.nextUrl
-const channel = url.searchParams.get('channel')
+  const url = request.nextUrl
+  const channel = url.searchParams.get('channel')
 
-const payload = `bluesky://intent/apply-ota?channel=${channel}`
+  const payload = `bluesky://intent/apply-ota?channel=${channel}`
 
-const qrCode = await QRCode.toBuffer(payload, { width: 1200 });
+  const qrCode = await QRCode.toBuffer(payload, { width: 1200 })
 
   return new Response(new Uint8Array(qrCode), {
     headers: {
       'Content-Type': 'image/png',
       'Cache-Control': 'public, max-age=3600',
     },
-  });
+  })
 }
